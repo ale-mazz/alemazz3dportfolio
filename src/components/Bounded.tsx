5c641e0f978c18b7269490afdfcb9cf8dd6f4f2d
Alessandro Mazzon <alessandro@forbole.com>
2024-02-19 20:45:51 +0100

    chore: initial commit with half of the homescreen

---
+++ src/components/Bounded.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import React, { forwardRef } from "react";
+
+type BoundedProps = {
+  as?: React.ElementType;
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Bounded = forwardRef<HTMLDivElement, BoundedProps>(
+  ({ as: Comp = "section", className, children, ...restProps }, ref) => {
+    return (
+      <Comp
+        ref={ref}
+        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+        {...restProps}
+      >
+        <div className="mx-auto w-full max-w-7xl">{children}</div>
+      </Comp>
+    );
+  },
+);
+
+Bounded.displayName = "Bounded";
+
+export default Bounded;
